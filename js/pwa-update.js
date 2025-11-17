3476327f5141f3d9ca8e6546bbecd302b083d565
grup4-lajoya <baselajoya@gmail.com>
2025-11-16 19:01:12 -0500

    Update pwa-update.js

--- js/pwa-update.js
+++ js/pwa-update.js
@@ -1,5 +1,5 @@
 // pwa-update.js - VERSIÓN MEJORADA
-const APP_VERSION = '3.0.1'; // ← Cambiar en cada actualización
+const APP_VERSION = '3.0.2'; // ← Cambiar en cada actualización
 const VERSION_KEY = 'app-version';
 
 const PROTECTED_KEYS = [
